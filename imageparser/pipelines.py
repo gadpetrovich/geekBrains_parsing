5ac523ce2454eac93be455abcfc4b824aed5c41c
Pavel Sharapov <gadpetrovich@gmail.com>
2024-12-23 04:08:36 +0300

    добавил загрузку фото и вывод в csv

--- imageparser/pipelines.py
+++ imageparser/pipelines.py
@@ -9,7 +9,24 @@ from itemadapter import ItemAdapter
 from pymongo import MongoClient
 from scrapy.pipelines.images import ImagesPipeline
 import scrapy
+import csv
 
-class ImageparserPipeline:
-    def process_item(self, item, spider):
+class ImageparserPipeline(ImagesPipeline):
+    def __init__(self, store_uri, download_func = None, settings = None, *, crawler = None):
+        self.csv_file = open('photos.csv', 'w', newline='\n', encoding='utf-8')
+        super().__init__(store_uri, download_func, settings, crawler=crawler)
+    def get_media_requests(self, item, info):
+        if item['photo']:
+            try:
+                yield scrapy.Request(item['photo'])
+            except Exception as e:
+                print(e)
+        #return super().get_media_requests(item, info)
+        
+    def item_completed(self, results, item, info):
+        if results:
+            item['photo'] = results[0][1]['path'] if results[0][0] else None
+        csv.writer(self.csv_file, delimiter=';').writerow([item['url'], item['title'], item['photo'], item['tags']])
         return item
+        #return super().item_completed(results, item, info)
+        
